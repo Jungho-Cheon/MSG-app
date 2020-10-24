45449d027661f8e080dd0dad7e67fdc75e834646
천정호 <cjsgh85@gmail.com>
2020-10-24 19:04:34 +0900

    remove config

--- servers/receipt_analysis/config.py
+++
@@ -1,4 +0,0 @@
-Config = {
-    'X-OCR-SECRET': 'amlkTXp4Z25sdmlVSW5ueVRSTUJybUhRdkhHVUdESlI=',
-    'OCR-END-POINT': "https://80db753d8aec48a6a1253004a26dc9a2.apigw.ntruss.com/custom/v1/2599/590d7d156869b57a1e42e2dcc5c7b84ad062b4899b4c57c479ded39907824d19/general"
-}
